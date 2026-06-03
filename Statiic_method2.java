5e963f592f973a9d980bee4cdfc5d5dc37b654ef
kambojanmol792-a11y <kambojanmol792@gmail.com>
2026-06-04 00:14:37 +0530

    Learning Git Files

---
+++ Statiic_method2.java
@@ -0,0 +1,20 @@
+
+public class Statiic_method2 {
+	static void test() 
+	{
+		int a = 40;
+		int b = 50;
+		
+		System.out.println(a+b);
+	}
+	
+	 public static void main(String[]args)
+	 {
+		 
+		 test();
+		 Static_method1.sum();
+		 Static_method1.mul();
+
+	}
+
+}
---
+++ Testcase2.java
@@ -0,0 +1,20 @@
+
+public class Testcase2 extends BaseClass {
+	
+	static void test() 
+	{
+		int a = 10;
+		int b = 2;
+		
+		int sub = a-b;
+		
+		System.out.println(sub);
+	}
+	
+	public static void main(String[]args) {
+		anmol();
+		
+		test();
+	}
+
+}
